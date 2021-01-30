29b1e737253a1b3b987e0faeb2d5adcf5b7ed472
Kaleemullah-Khan <Kaleempathan567@gmail.com>
2021-01-30 13:50:38 +0530

    pmu app two days work

--- src/Component/Body/MainBody.jsx
+++ src/Component/Body/MainBody.jsx
@@ -3,7 +3,7 @@ import {Col,Image} from "react-bootstrap"
 export const MainBody = () => {
     return (
         <div>
-            <Col xs={4} md={2}>
+            <Col xl={4} md={2}>
       <Image src="Assets/university.jpg" rounded/>
     </Col>
         </div>
